22d08def95c85984c2b540be27ca2cd981c685c0
Mehrdad Dehghan <m3hrdad.66@gmail.com>
2025-02-23 20:59:52 -0500

    Remove unused artifacts and update app.py for prediction functionality

--- app.py
+++ app.py
@@ -1,110 +1,116 @@
-import pickle
 from flask import Flask, request, jsonify, app, url_for, render_template
 import pandas as pd
 import numpy as np
+import joblib
 # from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
 from catboost import CatBoostRegressor
 
 
-
-#load data
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app = Flask(__name__) # create an app instance
-model = pickle.load(open('./models/Model.pkl', 'rb'))
-Brand_Encoder = pickle.load(open('./models/Brand_Encoder.pkl', 'rb'))
-Model_Encoder = pickle.load(open('./models/Model_Encoder.pkl', 'rb'))
-OneHot_Encoder = pickle.load(open('./models/OneHot_Encoder.pkl', 'rb'))
+model = joblib.load('./artifacts/Regressor_Model.pkl')
+Numerical_transformer = joblib.load('./artifacts/Numerical_Transformer.pkl')
+OneHot_encoder = joblib.load('./artifacts/OneHot_Encoder.pkl')
+Area_encoder = joblib.load('./artifacts/Area_Encoder.pkl')
+Price_transformer = joblib.load('./artifacts/Price_yeo_johnson_transformer.pkl') 
 
 
-
-@app.route('/',methods=['GET'])
-def Home():
-    return render_template('index.html')
+# @app.route('/',methods=['GET'])
+# def Home():
+#     return render_template('index.html')
 
 
 @app.route("/predict", methods=['POST'])
 def predict():
     if request.method == 'POST':
         # Extract form data
-        Brand = request.form['Brand'] 
-        Model = request.form['Model'] 
-        Fuel = request.form['Fuel'] 
-        Transmission = request.form['Transmission'] 
-        Year = int(request.form['Year']) 
-        EngineSize = float(request.form['EngineSize']) 
-        Mileage = int(request.form['Mileage']) 
-        Doors = int(request.form['Doors']) 
-        OwnerCount = int(request.form['OwnerCount']) 
+        Date = request.form['Date'] #از فرم به صورت تقویم
+        Area = request.form['Area']  #از فرم به صورت دراپ داون
+        Location = request.form['Location']  #از فرم به صورت دراپ داون
+        Type = request.form['Type'] #از فرم به صورت دراپ داون
+        MinDayNights = int(request.form['MinDayNights'])  #از فرم
+        CountReview = float(request.form['CountReview'])  #از فرم
+        AvgReview = int(request.form['AvgReview'])  #از فرم
+        TotalHostListings = int(request.form['TotalHostListings'])  #از فرم
+        DayAvailability = int(request.form['DayAvailability'])  #از فرم
 
         # Create a DataFrame for the input data
         input_df = pd.DataFrame({
-            'Brand': [Brand],
-            'Model': [Model],
-            'Year': [Year],
-            'EngineSize': [EngineSize],
-            'Fuel': [Fuel],
-            'Transmission': [Transmission],
-            'Mileage': [Mileage],
-            'Doors': [Doors],
-            'OwnerCount': [OwnerCount]
+            'LastDateReview': [Date],
+            'Area': [Area],
+            'Location': [Location],
+            'Type': [Type],
+            'MinDayNights': [MinDayNights],
+            'CountReview': [CountReview],
+            'AvgReview': [AvgReview],
+            'TotalHostListings': [TotalHostListings],
+            'DayAvailability': [DayAvailability]
         })
         
-        # Encode Brand and Model using mean target encoding
-        input_df['Encoded_Brand'] = input_df['Brand'].map(Brand_Encoder)
-        input_df['Encoded_Model'] = input_df['Model'].map(Model_Encoder)
-        input_df['Encoded_Brand'].fillna(input_df['Encoded_Brand'].mean(), inplace=True)
-        input_df['Encoded_Model'].fillna(input_df['Encoded_Model'].mean(), inplace=True)
-        input_df.drop(['Brand', 'Model'], axis=1, inplace=True)
         
-        # One-hot encode Fuel and Transmission
-        categorical_cols = ['Fuel', 'Transmission']
-        encoded_array = OneHot_Encoder.transform(input_df[categorical_cols])
-        encoded_df = pd.DataFrame(encoded_array, columns=OneHot_Encoder.get_feature_names_out(categorical_cols))
-        
-        # Merge encoded columns with input data
+        # Split Date for Year, Month, Day
+        input_df['LastDateReview'] = pd.to_datetime(input_df['LastDateReview'])
+        input_df['Year'] = input_df['LastDateReview'].dt.year
+        input_df['Month'] = input_df['LastDateReview'].dt.month
+        input_df['Day'] = input_df['LastDateReview'].dt.day
+        input_df.drop(columns=['LastDateReview'], inplace=True)
+
+
+        # Encode Month and Day
+        def encode(data, col, max_val):
+            data[col + '_sin'] = np.sin(2 * np.pi * data[col] / max_val)
+            data[col + '_cos'] = np.cos(2 * np.pi * data[col] / max_val)
+            return data
+
+        input_df = encode(input_df, 'Month', 12)
+        input_df = encode(input_df, 'Day', 31)
+        input_df = input_df.drop(columns=['Month', 'Day'])
+
+
+        # Encode Area
+        input_df['Area'] = input_df['Area'].map(Area_encoder)
+        input_df['Area'].fillna(input_df['Area'].mean(), inplace=True)
+
+
+        # Encode ocation & Type
+        categorical_cols = ['Location', 'Type']
+        encoded_array = OneHot_encoder.transform(input_df[categorical_cols])
+        encoded_df = pd.DataFrame(encoded_array, columns=OneHot_encoder.get_feature_names_out(categorical_cols))
         input_df_encoded = input_df.drop(columns=categorical_cols).reset_index(drop=True)
-        input_data = pd.concat([input_df_encoded, encoded_df], axis=1)
+        input_df = pd.concat([input_df_encoded, encoded_df], axis=1)
+
+
+        # Transform Numerical
+        columns_to_transform = ['MinDayNights' , 'CountReview' , 'AvgReview' , 'TotalHostListings' , 'DayAvailability']
+        input_df[columns_to_transform] = Numerical_transformer.transform(input_df[columns_to_transform])
+
+
+        # Scale Numerical 
+        columns_to_scale = ['MinDayNights', 'CountReview', 'AvgReview', 'TotalHostListings', 'DayAvailability', 'Year']
+        scaler = ColumnTransformer([('scaler', StandardScaler(), columns_to_scale)], remainder='passthrough')
+        X_scaled = scaler.fit_transform(input_df)
+        all_columns = columns_to_scale + [col for col in input_df.columns if col not in columns_to_scale]
+        X_scaled_df = pd.DataFrame(X_scaled, columns=all_columns)
+        input_df = X_scaled_df.copy()
+
+
+        # ReOrder
+        input_df = input_df[['MinDayNights', 'CountReview', 'AvgReview', 'TotalHostListings',
+                            'DayAvailability', 'Year', 'Area', 'Location_Brooklyn', 'Location_Manhattan', 
+                            'Location_Queens', 'Location_Staten Island', 'Type_Private room', 'Type_Shared room', 
+                            'Month_sin', 'Month_cos', 'Day_sin', 'Day_cos']]
+
         
         # Make prediction
-        prediction = model.predict(input_data)
-        output = round(prediction[0], 2)
+        prediction = model.predict(input_df)
+        prediction_original = Price_transformer.inverse_transform(prediction.reshape(-1, 1))
+        output = round(prediction_original[0], 2)
         
         if output < 0:
-            return render_template('index.html', prediction_texts="Sorry you cannot sell this car")
+            return render_template('index.html', prediction_texts="Sorry you cannot rent your room")
         else:
-            return render_template('index.html', prediction_text="Car is worth at: $ {}".format(output))
+            return render_template('index.html', prediction_text="You can rent at: $ {}".format(output))
     else:
         return render_template('index.html')
         
